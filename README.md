11c66d929234f6d0c8b225f413bb194ea61c2319
Miles Stanfield <miles.stanfield@turner.com>
2016-01-08 20:09:20 -0500

    seperated styles to be required one by one, updated specs and readme

--- README.md
+++ README.md
@@ -20,7 +20,9 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+<pre>
+  *= require "grids_lite"
+</pre>
 
 ## Development
 
--- spec/features/grids_lite_spec.rb
+++ spec/features/grids_lite_spec.rb
@@ -1,8 +1,40 @@
 require "spec_helper"
 
 describe "GridsLite", type: :feature do
-  it "provides grids_lite.css on the asset pipeline" do
-    visit "app/assets/stylesheets/grids_lite.css"
-    expect(page.body).to include ".foo {"
+  context "asset pipeline stylesheets" do
+    it "has a base css file" do
+      visit "app/assets/stylesheets/grids_lite/base.css"
+      expect(page.body).to include "body {"
+    end
+
+    it "has a col-2 css file" do
+      visit "app/assets/stylesheets/grids_lite/col-2.css"
+      expect(page.body).to include ".col-2-2"
+    end
+
+    it "has a col-3 css file" do
+      visit "app/assets/stylesheets/grids_lite/col-3.css"
+      expect(page.body).to include ".col-3-3"
+    end
+
+    it "has a col-4 css file" do
+      visit "app/assets/stylesheets/grids_lite/col-4.css"
+      expect(page.body).to include ".col-4-4"
+    end
+
+    it "has a col-5 css file" do
+      visit "app/assets/stylesheets/grids_lite/col-5.css"
+      expect(page.body).to include ".col-5-5"
+    end
+
+    it "has a col-6 css file" do
+      visit "app/assets/stylesheets/grids_lite/col-6.css"
+      expect(page.body).to include ".col-6-6"
+    end
+
+    it "has a col-8 css file" do
+      visit "app/assets/stylesheets/grids_lite/col-8.css"
+      expect(page.body).to include ".col-8-8"
+    end
   end
 end
